f7035928d5cefddcb562d5201c1af9752341acfa
n-renz0801 <n.renz.domingo@gmail.com>
2026-04-12 19:31:11 +0800

    first deploy

---
+++ app.py
@@ -0,0 +1,203 @@
+from flask import Flask, render_template, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask import session
+
+app = Flask(__name__)
+app.secret_key = 'renzqtpie'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(100), unique=True, nullable=False)
+    password = db.Column(db.String(200), nullable=False)
+
+    expenses = db.relationship('Expense', backref='user', lazy=True)
+
+class Expense(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    amount = db.Column(db.Float)
+    description = db.Column(db.String(300))
+    date = db.Column(db.DateTime, default=datetime.utcnow)
+
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    def __repr__(self):
+        return '<Expense %r>' % self.id
+    
+# @app.route('/')
+# def home():
+#     return render_template('home.html')  # landing page
+
+@app.route('/expenses')
+def expenses():
+    if 'user_id' not in session:
+        return redirect('/login')
+
+    expenses = Expense.query.filter_by(user_id=session['user_id']).order_by(Expense.date).all()
+    return render_template('index.html', expenses=expenses)
+
+@app.route('/add_expense', methods=['GET', 'POST'])
+def add_expense():
+    if request.method == 'POST':
+        amount = float(request.form['amount'])
+        description = request.form['description']
+        date_str = request.form['date']
+
+        # convert string to datetime
+        date = datetime.strptime(date_str, '%Y-%m-%d')
+
+        new_expense = Expense(
+            amount=amount,
+            description=description,
+            date=date,
+            user_id=session['user_id']
+        )
+
+        db.session.add(new_expense)
+        db.session.commit()
+
+        return redirect('/expenses')
+
+    today = datetime.today().strftime('%Y-%m-%d')
+    return render_template('add_expense.html', today=today)
+
+@app.route('/user-expenses', methods=['GET'])
+def user_expenses():
+    users = User.query.all()
+
+    selected_user_id = request.args.get('user_id')
+
+    expenses = []
+    data = {}
+
+    if selected_user_id:
+        expenses = Expense.query.filter_by(user_id=selected_user_id).all()
+
+        for expense in expenses:
+            date = expense.date.strftime('%Y-%m-%d')
+
+            if date not in data:
+                data[date] = 0
+
+            data[date] += expense.amount
+
+    sorted_dates = sorted(data.keys())
+
+    return render_template(
+        'user_expenses.html',
+        users=users,
+        data=data,
+        dates=sorted_dates,
+        selected_user_id=selected_user_id
+    )
+
+
+@app.route('/edit/<int:id>', methods=['GET', 'POST'])
+def edit_expense(id):
+    expense = Expense.query.get_or_404(id)
+
+    if request.method == 'POST':
+        expense.amount = float(request.form['amount'])
+        expense.description = request.form['description']
+        expense.date = datetime.strptime(request.form['date'], '%Y-%m-%d')
+
+        db.session.commit()
+        return redirect('/expenses')
+
+    return render_template('edit.html', expense=expense)
+
+
+@app.route('/delete/<int:id>')
+def delete_expense(id):
+    expense = Expense.query.get_or_404(id)
+
+    db.session.delete(expense)
+    db.session.commit()
+
+    return redirect('/expenses')
+
+
+@app.route('/dashboard')
+def dashboard():
+    expenses = Expense.query.all()
+
+    # structure:
+    # {date: {user: total}}
+    data = {}
+    users = set()
+
+    for expense in expenses:
+        date = expense.date.strftime('%Y-%m-%d')
+        user = expense.user.username
+        
+        users.add(user)
+
+        if date not in data:
+            data[date] = {}
+
+        if user not in data[date]:
+            data[date][user] = 0
+
+        data[date][user] += expense.amount
+
+    # sort dates
+    sorted_dates = sorted(data.keys())
+
+    # sort users
+    users = sorted(users)
+
+    return render_template(
+        'dashboard.html',
+        data=data,
+        dates=sorted_dates,
+        users=users
+    )
+
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = generate_password_hash(request.form['password'])
+
+        user = User(username=username, password=password)
+
+        db.session.add(user)
+        db.session.commit()
+
+        return redirect('/login')
+
+    return render_template('register.html')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        user = User.query.filter_by(username=username).first()
+
+        if user and check_password_hash(user.password, password):
+            session['user_id'] = user.id
+            return redirect('/expenses')
+        else:
+            error = "Invalid username or password"
+
+    return render_template('login.html', error=error)
+
+
+@app.route('/logout')
+def logout():
+    session.pop('user_id', None)
+    return redirect('/login')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
